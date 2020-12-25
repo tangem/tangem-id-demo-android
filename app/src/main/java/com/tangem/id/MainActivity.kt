dc4ee51031c5709bc3704d71782326353d56c43f
Denis Baturin <dbaturin@tangem.com>
2020-12-25 19:03:55 +0300

    backend update

--- app/src/main/java/com/tangem/id/MainActivity.kt
+++ app/src/main/java/com/tangem/id/MainActivity.kt
@@ -31,6 +31,9 @@ private lateinit var presentationService: PresentationService
 private lateinit var presentationRequest: PresentationRequest
 private lateinit var issuanceResponse: IssuanceResponse
 private lateinit var requestedToken: IdTokenAttestation
+private lateinit var serviceConfig: AuthorizationServiceConfiguration
+private lateinit var authService: AuthorizationService
+private val codeVerifier = "0123456789_0123456789_0123456789_0123456789"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,6 +78,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun testMicrosoftVc() {
+        authService = AuthorizationService(this)
         CoroutineScope(Job()).launch {
 //            val publicKey = "0456C024AFE6CB2C1A384736D5C2AF008C8B6DA7256976C5ADE723E3B05087ED8C58E334A9DC53AA42E3821BA186307C6A9DABED6E8F72612ECFE22FEE0419E0B6".hexToBytes()
 //            val identifierString = TangemIonIdentifierCreator().create(publicKey)
@@ -92,7 +96,7 @@ class MainActivity : AppCompatActivity() {
 //            }
 
             presentationRequest = when (val result = presentationService.getRequest(
-                "openid://vc/?request_uri=https%3A%2F%2Fvc.tangem.com%2Fissue-request.jwt%3Fid%3D8fmXLQAyvzKB1FpvANlAfDKHq5pyzBOm"
+                "openid://vc/?request_uri=https%3A%2F%2Fvc.tangem.com%2Fissue-request.jwt%3Fid%3DclbQl7GWojUXE5WkIa9VNdDv-5vL-Kgw"
             )) {
                 is MSResult.Success -> result.payload
                 is MSResult.Failure -> return@launch
@@ -128,73 +132,73 @@ class MainActivity : AppCompatActivity() {
 //            )
 
             AuthorizationServiceConfiguration.fetchFromUrl(
-                Uri.parse(requestedToken.configuration),
-                AuthorizationServiceConfiguration
-                    .RetrieveConfigurationCallback { serviceConfig, exception ->
-                        val authRequestBuilder = AuthorizationRequest.Builder(
-                            serviceConfig!!,  // the authorization service configuration
-                            requestedToken.client_id,  // the client ID, typically pre-registered and static
-                            ResponseTypeValues.ID_TOKEN,  // the response_type value: we want a code
-                            Uri.parse(requestedToken.redirect_uri) // the redirect URI to which the auth response is sent
-                        )
+                Uri.parse(requestedToken.configuration)
+            ) { _serviceConfig, exception ->
+                serviceConfig = _serviceConfig!!
+                val authRequestBuilder = AuthorizationRequest.Builder(
+                    serviceConfig!!,  // the authorization service configuration
+                    requestedToken.client_id,  // the client ID, typically pre-registered and static
+                    ResponseTypeValues.CODE,  // the response_type value: we want a code
+                    Uri.parse(requestedToken.redirect_uri) // the redirect URI to which the auth response is sent
+                )
 
-                        val authRequest = authRequestBuilder
-                            .setScope("openid")
-                            .setResponseMode("query")
-                            .build()
+                val authRequest = authRequestBuilder
+                    .setScope("openid")
+                    .setResponseMode("query")
+                    .setCodeVerifier("0123456789_0123456789_0123456789_0123456789")
+                    .build()
 
-                        doAuthorization(authRequest)
-                    }
-            )
-
-//            val idToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjdfWnVmMXR2a3dMeFlhSFMzcTZsVWpVWUlHdyIsImtpZCI6IjdfWnVmMXR2a3dMeFlhSFMzcTZsVWpVWUlHdyJ9.eyJhdWQiOiJiMTRhNzUwNS05NmU5LTQ5MjctOTFlOC0wNjAxZDBmYzljYWEiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9mYTE1ZDY5Mi1lOWM3LTQ0NjAtYTc0My0yOWYyOTU2ZmQ0MjkvIiwiaWF0IjoxNTM2Mjc1MTI0LCJuYmYiOjE1MzYyNzUxMjQsImV4cCI6MTUzNjI3OTAyNCwiYWlvIjoiQVhRQWkvOElBQUFBcXhzdUIrUjREMnJGUXFPRVRPNFlkWGJMRDlrWjh4ZlhhZGVBTTBRMk5rTlQ1aXpmZzN1d2JXU1hodVNTajZVVDVoeTJENldxQXBCNWpLQTZaZ1o5ay9TVTI3dVY5Y2V0WGZMT3RwTnR0Z2s1RGNCdGsrTExzdHovSmcrZ1lSbXY5YlVVNFhscGhUYzZDODZKbWoxRkN3PT0iLCJhbXIiOlsicnNhIl0sImVtYWlsIjoiYWJlbGlAbWljcm9zb2Z0LmNvbSIsImZhbWlseV9uYW1lIjoiTGluY29sbiIsImdpdmVuX25hbWUiOiJBYmUiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDcvIiwiaXBhZGRyIjoiMTMxLjEwNy4yMjIuMjIiLCJuYW1lIjoiYWJlbGkiLCJub25jZSI6IjEyMzUyMyIsIm9pZCI6IjA1ODMzYjZiLWFhMWQtNDJkNC05ZWMwLTFiMmJiOTE5NDQzOCIsInJoIjoiSSIsInN1YiI6IjVfSjlyU3NzOC1qdnRfSWN1NnVlUk5MOHhYYjhMRjRGc2dfS29vQzJSSlEiLCJ0aWQiOiJmYTE1ZDY5Mi1lOWM3LTQ0NjAtYTc0My0yOWYyOTU2ZmQ0MjkiLCJ1bmlxdWVfbmFtZSI6IkFiZUxpQG1pY3Jvc29mdC5jb20iLCJ1dGkiOiJMeGVfNDZHcVRrT3BHU2ZUbG40RUFBIiwidmVyIjoiMS4wIn0=.UJQrCA6qn2bXq57qzGX_-D3HcPHqBMOKDPx4su1yKRLNErVD8xkxJLNLVRdASHqEcpyDctbdHccu6DPpkq5f0ibcaQFhejQNcABidJCTz0Bb2AbdUCTqAzdt9pdgQvMBnVH1xk3SCM6d4BbT4BkLLj10ZLasX7vRknaSjE_C5DI7Fg4WrZPwOhII1dB0HEZ_qpNaYXEiy-o94UJ94zCr07GgrqMsfYQqFR7kn-mn68AjvLcgwSfZvyR_yIK75S_K37vC3QryQ7cNoafDe9upql_6pB2ybMVlgWPs_DmbJ8g0om-sPlwyn74Cc1tW3ze-Xptw_2uVdPgWyqfuWAfq6Q"
-//            response.requestedIdTokenMap[requestedToken.configuration] = idToken
-//
-//
-//            val vch = when (val result =
-//                verifiableCredentialManager.sendIssuanceResponse(response)) {
-//                is MSResult.Success -> result.payload
-//                is MSResult.Failure -> return@launch
-//            }
+                doAuthorization(authRequest)
+            }
         }
 
 
     }
 
     private fun doAuthorization(authRequest: AuthorizationRequest) {
-        val authService = AuthorizationService(this)
         val authIntent: Intent = authService.getAuthorizationRequestIntent(authRequest)
         startActivityForResult(authIntent, 0)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        runBlocking {
             super.onActivityResult(requestCode, resultCode, data)
             if (requestCode == 0) {
                 val resp = AuthorizationResponse.fromIntent(data!!)
                 val ex = AuthorizationException.fromIntent(data)
 
-//                val idToken =
-//                    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjdfWnVmMXR2a3dMeFlhSFMzcTZsVWpVWUlHdyIsImtpZCI6IjdfWnVmMXR2a3dMeFlhSFMzcTZsVWpVWUlHdyJ9.eyJhdWQiOiJiMTRhNzUwNS05NmU5LTQ5MjctOTFlOC0wNjAxZDBmYzljYWEiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9mYTE1ZDY5Mi1lOWM3LTQ0NjAtYTc0My0yOWYyOTU2ZmQ0MjkvIiwiaWF0IjoxNTM2Mjc1MTI0LCJuYmYiOjE1MzYyNzUxMjQsImV4cCI6MTUzNjI3OTAyNCwiYWlvIjoiQVhRQWkvOElBQUFBcXhzdUIrUjREMnJGUXFPRVRPNFlkWGJMRDlrWjh4ZlhhZGVBTTBRMk5rTlQ1aXpmZzN1d2JXU1hodVNTajZVVDVoeTJENldxQXBCNWpLQTZaZ1o5ay9TVTI3dVY5Y2V0WGZMT3RwTnR0Z2s1RGNCdGsrTExzdHovSmcrZ1lSbXY5YlVVNFhscGhUYzZDODZKbWoxRkN3PT0iLCJhbXIiOlsicnNhIl0sImVtYWlsIjoiYWJlbGlAbWljcm9zb2Z0LmNvbSIsImZhbWlseV9uYW1lIjoiTGluY29sbiIsImdpdmVuX25hbWUiOiJBYmUiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDcvIiwiaXBhZGRyIjoiMTMxLjEwNy4yMjIuMjIiLCJuYW1lIjoiYWJlbGkiLCJub25jZSI6IjEyMzUyMyIsIm9pZCI6IjA1ODMzYjZiLWFhMWQtNDJkNC05ZWMwLTFiMmJiOTE5NDQzOCIsInJoIjoiSSIsInN1YiI6IjVfSjlyU3NzOC1qdnRfSWN1NnVlUk5MOHhYYjhMRjRGc2dfS29vQzJSSlEiLCJ0aWQiOiJmYTE1ZDY5Mi1lOWM3LTQ0NjAtYTc0My0yOWYyOTU2ZmQ0MjkiLCJ1bmlxdWVfbmFtZSI6IkFiZUxpQG1pY3Jvc29mdC5jb20iLCJ1dGkiOiJMeGVfNDZHcVRrT3BHU2ZUbG40RUFBIiwidmVyIjoiMS4wIn0=.UJQrCA6qn2bXq57qzGX_-D3HcPHqBMOKDPx4su1yKRLNErVD8xkxJLNLVRdASHqEcpyDctbdHccu6DPpkq5f0ibcaQFhejQNcABidJCTz0Bb2AbdUCTqAzdt9pdgQvMBnVH1xk3SCM6d4BbT4BkLLj10ZLasX7vRknaSjE_C5DI7Fg4WrZPwOhII1dB0HEZ_qpNaYXEiy-o94UJ94zCr07GgrqMsfYQqFR7kn-mn68AjvLcgwSfZvyR_yIK75S_K37vC3QryQ7cNoafDe9upql_6pB2ybMVlgWPs_DmbJ8g0om-sPlwyn74Cc1tW3ze-Xptw_2uVdPgWyqfuWAfq6Q"
-                issuanceResponse.requestedIdTokenMap[requestedToken.configuration] =
-                    resp!!.idToken!!
+                val code = resp?.authorizationCode
+                val tokenRequestBuilder = TokenRequest.Builder(
+                    serviceConfig!!,  // the authorization service configuration
+                    requestedToken.client_id,  // the client ID, typically pre-registered and static
+                )
 
+                val tokenRequest = tokenRequestBuilder
+                    .setScope("openid")
+                    .setAuthorizationCode(code)
+                    .setGrantType("authorization_code")
+                    .setRedirectUri(Uri.parse(requestedToken.redirect_uri))
+                    .setCodeVerifier(codeVerifier)
+                    .build()
 
-                val verifiableCredential = when (val result =
-                    issuanceService.sendResponse(issuanceResponse)) {
-                    is MSResult.Success -> result.payload
-                    is MSResult.Failure -> return@runBlocking
-                }
+                authService.performTokenRequest(tokenRequest) { response, exception ->
+                    issuanceResponse.requestedIdTokenMap[requestedToken.configuration] =
+                        response!!.idToken!!
+                    runBlocking {
+                        val verifiableCredential = when (val result =
+                            issuanceService.sendResponse(issuanceResponse)) {
+                            is MSResult.Success -> result.payload
+                            is MSResult.Failure -> return@runBlocking
+                        }
 
-                val presentationResponse = PresentationResponse(presentationRequest)
-                val requestedVc = presentationRequest.getPresentationDefinition()
-                    .credentialPresentationInputDescriptors[0]
-                presentationResponse.requestedVcPresentationSubmissionMap[requestedVc] =
-                    verifiableCredential
+                        val presentationResponse = PresentationResponse(presentationRequest)
+                        val requestedVc = presentationRequest.getPresentationDefinition()
+                            .credentialPresentationInputDescriptors[0]
+                        presentationResponse.requestedVcPresentationSubmissionMap[requestedVc] =
+                            verifiableCredential
 
-
-                val result = presentationService.sendResponse(presentationResponse)
-                print(result)
+                        val result = presentationService.sendResponse(presentationResponse)
+                        print(result)
+                    }
             }
         }
     }
